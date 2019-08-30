1c447ea140cf5012bd107af7890b4ede6e71b321
Chris Martin <chris@sensedev.com>
2019-08-30 14:54:30 -0700

    testing tests

--- test_user_model.py
+++ test_user_model.py
@@ -4,7 +4,6 @@
 #
 #    python -m unittest test_user_model.py
 
-
 import os
 from unittest import TestCase
 
@@ -16,7 +15,6 @@ from models import db, User, Message, Follows
 # connected to the database
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
-
 
 # Now we can import app
 
@@ -31,24 +29,50 @@ db.create_all()
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
-
     def setUp(self):
         """Create test client, add sample data."""
 
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
-        
-        self.u = User.signup(        
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD",
-            image_url=None
-        )
+
+        # Setup users
+        self.u = User.signup(email="test@test.com",
+                             username="testuser",
+                             password="HASHED_PASSWORD",
+                             image_url=None)
+
+        self.u2 = User.signup(email="test2@test.com",
+                              username="testuser2",
+                              password="HASHED_PASSWORD",
+                              image_url=None)
+
+        self.u3 = User.signup(email="test3@test.com",
+                              username="testuser3",
+                              password="HASHED_PASSWORD",
+                              image_url=None)
         self.u.id = 1
+        self.u2.id = 2
+        self.u3.id = 3
+        
+        
+        # add follower
+        self.u.following.append(self.u2)
+        
+        # commit DB
         db.session.commit()
-        self.client = app.test_client()       
-    
+        self.client = app.test_client()
+
     def test_user_repr(self):
         """ Test user repr function"""
         self.assertEqual(str(self.u), "<User #1: testuser, test@test.com>")
+
+    def test_user_is_following(self):
+        """ Test user is following user 3"""
+        self.assertEqual(User.is_followed_by(self.u2, self.u), True)
+        self.assertEqual(User.is_followed_by(self.u2, self.u3), False)
+
+    def test_user_is_not_following(self):
+        """ Test user not following user 2"""
+        self.assertEqual(User.is_following(self.u, self.u3), False)
+        self.assertEqual(User.is_followed_by(self.u2, self.u), True)
